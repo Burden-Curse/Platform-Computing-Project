ca4bfbc5c4a244b26f3c9008d8360826e8a395b4
Ken <58831340+Burden-Curse@users.noreply.github.com>
2024-05-14 22:23:13 -0700

    Add files via upload

--- metric_tracker.py
+++ metric_tracker.py
@@ -17,7 +17,7 @@ def WritetoCSV(filename : str, metrics : dict):
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Gerlie100", #Blank on purpose
+    passwd="", #Blank on purpose
     database="cse4500"
 )
 
